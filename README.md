bf652c0c464b547019d5d3e97eea53dd7305a289
CK Hung <hungchongki@gmail.com>
2023-03-27 17:46:19 +0800

    init version.

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # vanilla-chatgpt
-vanilla ChatGPT client, run from local or any static web host
+
+A minimal ChatGPT client by vanilla javascript, run from local or any static web host
+
+For example, 
+
+* https://github.com/casualwriter/vanilla-chatgpt/main/source/index.html
+* https://github.com/casualwriter/vanilla-chatgpt/main/source/vanilla-chatgpt.html
+
+![](vanilla-chatgpt.jpg)
+
+### Features
+
+* no dependence in vanilla javascript
+* no installation, run from local or web
+* simple with reusable library
+* print friendly for conversation
+* responsive for mobile
+
+### How to run and use
+
+Fork [this repo](https://github.com/casualwriter/vanilla-chatgpt), 
+or just click on above link, or download and run locally.
+
+1. ``input OPEN_API_KEY`` when load the page first time. API key will save to browser local storage.
+1. ``Prompt Hints`` shows in right-corner, click to select it.
+1. ``Input prompt`` and click `[Send]` button or press `ctrl-enter`
+1. ``List of prompts`` will show in right corner, click to jump to prompt section.
+1. Press [ctrl-p] to `print conversation`
+1. Click on [clear] button, to `clear conversation history`
+1. Click on **[logout]** button, to `clear OPEN_API_KEY` from local storage.
+
+
+### Files
+
+This repository include 2 version of vanilla-chatGPT, and its javascript libary
+
+1. `index.html` is the all-in-one version of vanilla-chatGPT. no dependence in vanilla javascript.
+2. `vanilla-chatgpt.html` is the minimum version using casual-markdown.js and vanilla-chatgpt.js.
+3. `casual-markdown@0.91.js`  // [casual-markdown 0.91](https://raw.githack.com/casualwriter/casual-markdown), for chatGPT markdown
+4. `vanilla-chatgpt.js`  //  js library for vanilla-chatgpt, reusable for your chatGPT client.
+
+
+### Credit
+
+Inspired by the some self-host solution
+
+* https://github.com/yuezk/chatgpt-mirror, mirror of ChatGPT (node.js, self-host)
+* https://github.com/dirk1983/chatgpt, a simple self-host solution (php)
+
+
+(2023/03/27, v0.50, casualwriter, MIT license)
---
+++ source/vanilla-chatgpt.js
@@ -0,0 +1,106 @@
+﻿/*****************************************************************************
+* vanilla-chatgpt.js - chat library for openai-chatgpt
+* last updated on 2023/03/27, v0.50, basic chat, responsive, print-friendly.
+*
+* Copyright (c) 2023, Casualwriter (MIT Licensed)
+* https://github.com/casualwriter/vanilla-chatgpt
+*****************************************************************************/
+
+const chat = (id) => window.document.getElementById(id);
+
+// Set the API endpoint URL
+chat.endPoint  = "https://api.openai.com/v1/chat/completions";
+chat.model = "gpt-3.5-turbo"
+chat.body  = { model: chat.model, temperature: 0.8 }
+chat.history = []
+
+// stream result from openai
+chat.stream = function (prompt) {
+
+  chat.body.stream = true 
+  chat.body.messages = [ { role: "user", content: prompt} ]
+  chat.headers = { "Authorization": `Bearer ${chat.apiKey}`, "Content-Type": "application/json" }
+  chat.result = ''
+  chat.controller = new AbortController();
+  const signal = chat.controller.signal
+   
+  for (let i=chat.history.length-1; i>=0&&i>(chat.history.length-3); i--) {
+    chat.body.messages.unshift( { role:'assistant', content: chat.history[i].result } );
+    chat.body.messages.unshift( { role:'user', content: chat.history[i].prompt } );
+  }
+  
+  fetch( chat.endPoint, { method:'POST', headers: chat.headers, body: JSON.stringify(chat.body), signal } )
+  .then( response => { 
+  
+    if (!response.ok) {
+        if (response.status == 401) throw new Error('401 Unauthorized, invalide API Key');
+        throw new Error('failed to get data, error status '+response.status)
+    }
+    
+    const reader = response.body.pipeThrough(new TextDecoderStream()).getReader();
+    
+    reader.read().then( function processText({ done, value }) {
+    
+      if (done) return chat.oncomplete(chat.result);
+      
+      const lines = (chat.value=value).split('\n');
+
+      for (let i in lines) {
+        if (lines[i].length === 0) continue;     // ignore empty message
+        if (lines[i].startsWith(':')) continue;  // ignore comment message
+        if (lines[i] === 'data: [DONE]') return chat.oncomplete(chat.result); // end of message
+        
+        chat.json = JSON.parse(lines[i].substring(6));
+        if (chat.json.choices) {
+          chat.result += (chat.json.choices[0].delta.content||'') 
+        }	 
+      }
+
+      chat.onmessage(chat.result)
+      return reader.read().then(processText);
+       
+    } )
+    
+  } ).catch( error => chat.onerror(error) );
+  
+}
+    
+//===== send prompt to chatGPT (not used in this program)
+chat.send = function (prompt) {
+  // Set the request data
+  const data = JSON.stringify({
+      model: "gpt-3.5-turbo", 
+      max_tokens: 1000,
+      temperature: 0.2, 
+      messages:   [ { role: "user", content: prompt} ]
+  });
+
+  // Make the API call
+  fetch( chat.endPoint, { method: "POST", headers: chat.headers,  body: data, })
+  .then(response => response.json() )
+  .then(json => {
+     chat.json = json
+     if (json.choices) {
+        chat.result += json.choices[0].message.content + '\n\n'        
+        chat.onmessage(chat.result)
+        chat.oncomplete(chat.result)
+     }	 
+  })
+  .catch(error => console.error(error));
+}
+
+// default error handle
+chat.onerror = (error) => { alert(error);  };
+
+// clear API key when logout
+chat.logout = () => { 
+  if (confirm( 'Logout and clear API Key?')) localStorage.clear();
+}
+
+// clear conversation history
+chat.clear = () => { 
+  if (confirm( 'Clear chat history?')) {
+    chat.history= [] 
+    chat("list").innerHTML = chat("main").innerHTML = ''
+  }
+}  
